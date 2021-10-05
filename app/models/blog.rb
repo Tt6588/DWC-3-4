70c59b8ece85061f34f444974aeac277992fb9eb
Tt6588 <tatsusato1011@gmail.com>
2021-10-05 07:28:33 +0000

    [finish]3-8

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,6 @@ class Blog < ApplicationRecord
   
   validates :title, presence: true
   validates :category, presence: true
-  validetes :body, presence: true
+  validates :body, presence: true
   
 end
